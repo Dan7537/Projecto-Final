e6b8178a35cd1ca763823faf45693197fec98442
wolflo <londonolondonoj@gmail.com>
2022-11-24 18:22:30 -0500

    Projecto de POO-Programa que recibe informacion de empleados y los guarda en un archivo de texto

---
+++ FinalProject.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.finalproject;
+
+import com.mycompany.finalproject.MainJFrame;
+
+/**
+ *
+ * @author daniel
+ */
+public class FinalProject {
+
+	
+    public static void main(String[] args) {
+        //System.out.println("Hello World!");
+        new MainJFrame();
+        
+    }
+}
